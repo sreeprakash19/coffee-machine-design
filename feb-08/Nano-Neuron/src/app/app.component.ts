7f887ff16b19287ff66ff2615e29ec8db3e2274d
sreeprakash19 <sreeprakash1995@gmail.com>
2020-02-10 14:50:19 +0530

    template finished

--- feb-08/Nano-Neuron/src/app/app.component.ts
+++ feb-08/Nano-Neuron/src/app/app.component.ts
@@ -1,34 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Nano-Neuron';
-  epochs = ['input_epochs'];
+  epochs = [];
+  epoch=[];
   predict;
   alpha = 0.0005;
   datasets=[];
+  xfield ='xtrain';
+  yfield='ytrain'
+  NanoNeurons={}
+  trained_model = {};
+  loadingdata={};
+  tempInCelsius = 70;
+  
+  public data: any;
+  public layout: any;
 
  // nanoNeuron = new NanoNeuron(w, b);
   
-
+ constructor(private httpClient: HttpClient) {}
+    ngOnInit()
+    {}
   
-  generateData(newValue)
-   { 
-      this.datasets.push(newValue);
-      console.log(this.datasets)
-  }
+    generateData(newValue)
+     { 
+       this.datasets.push(newValue);
+        console.log('value pushed:',this.datasets)
+     }
 
     NanoNeuron(w, b)
-    {
+     {
       w = w;
       b = b;
       this.predict = (x) => {
-        return x * w + b;
+      return x * w + b;
     }
 }
      celsiusToFahrenheit(c) {
@@ -38,86 +50,97 @@ export class AppComponent {
        return f;
       }
 
+     // const [xTrain, yTrain, xTest, yTest] = generateDataSets();
 
-      generatedatasets()
+  generatedatasets()
       {
        const xTrain = [];
-       const yTrain = [];
-       
-       for (let x = 0; x < 10; x += 1) {
-        const y =this. celsiusToFahrenheit(x);
-        xTrain.push(x);
-        yTrain.push(y);
-      }
-      const xTest = [];
-      const yTest = [];
-      for (let x = 0.5; x < 5; x += 1) {
-        const y = this.celsiusToFahrenheit(x);
-        xTest.push(x);
-        yTest.push(y);
-      }
-      return [xTrain, yTrain, xTest, yTest];
-      }
+         const yTrain = [];
+          for (let x = 0; x < 10; x += 1) {
+            const y =this. celsiusToFahrenheit(x);
+             xTrain.push(x);
+               yTrain.push(y);
+             }
+              const xTest = [];
+               const yTest = [];
+                 for (let x = 0.5; x < 5; x += 1)
+                    {
+                      const y = this.celsiusToFahrenheit(x);
+                        xTest.push(x);
+                         yTest.push(y);
+                     }
+                      return [xTrain, yTrain, xTest, yTest];
+                     }
 
 
-      forwardPropagation(model, xTrain, yTrain)
+     forwardPropagation(model, xTrain, yTrain)
       {
         let predictionCost;
-        let nanoNeuron
-       const m = xTrain.length;
-       const predictions = [];
-       let cost = 0;
-       for (let i = 0; i < m; i += 1) {
-        const prediction = nanoNeuron.predict(xTrain[i]);
-        cost += predictionCost(yTrain[i], prediction);
-      //  console.log('cost1',cost)
-        predictions.push(prediction);
-        console.log('prediction forward' ,prediction    )
+         let nanoNeuron
+          const m = xTrain.length;
+           const predictions = [];
+            let cost = 0;
+             for (let i = 0; i < m; i += 1) {
+              const prediction = nanoNeuron.predict(xTrain[i]);
+               cost += predictionCost(yTrain[i], prediction);
+        //  console.log('cost1',cost)
+                 predictions.push(prediction);
+                   console.log('prediction forward' ,prediction    )
         
       }
-      cost /= m;
-      console.log('cost    valueeee is :',cost)
-      return [predictions, cost];
-
+        cost /= m;
+        console.log('cost    valueeee is :',cost)
+        return [predictions, cost];
       }
-      backwardPropagation(predictions, xTrain, yTrain) 
-      {
-        const m = xTrain.length;
-        let dW = 0;
-        let dB = 0;
-        for (let i = 0; i < m; i += 1){
-          
-        dW += (yTrain[i] - predictions[i]) * xTrain[i];
-        console.log(' Dw7777777777777', dW += (yTrain[i] - predictions[i]) * xTrain[i])
-      
-        dB += yTrain[i] - predictions[i];
-        console.log('dB***********', dB += yTrain[i] - predictions[i])
+       backwardPropagation(predictions, xTrain, yTrain) 
+       {
+         const m = xTrain.length;
+         let dW = 0;
+         let dB = 0;
+         for (let i = 0; i < m; i += 1){
+         dW += (yTrain[i] - predictions[i]) * xTrain[i];
+         console.log(' Dw7777777777777', dW += (yTrain[i] - predictions[i]) * xTrain[i])
+         dB += yTrain[i] - predictions[i];
+         console.log('dB***********', dB += yTrain[i] - predictions[i])
 
-    }
+       }
     
-    dW /= m;
-    console.log('average dw', dW  )
+       dW /= m;
+       console.log('average dw', dW  )
 
-    dB /= m;
-    console.log('AVERAGE DB',  dB )
-    return [dW, dB];
-  
-    }
-    async trainModel({model, epochs, alpha, xTrain, yTrain})
-    {
-      let nanoNeuron;
-      const costHistory = [];
-      for (let epoch = 0; epoch < epochs; epoch += 1){
-        const [predictions, cost] = this.forwardPropagation(model, xTrain, yTrain);
-        costHistory.push(cost);
-        
-       const [dW, dB] = this.backwardPropagation(predictions, xTrain, yTrain);
-       nanoNeuron.w += alpha * dW;
-       console.log('nanoNeuron dw',nanoNeuron.w)
-       nanoNeuron.b += alpha * dB;
-       console.log('nanoNeuron dB',nanoNeuron.b)
-     }
-     return costHistory;
+       dB /= m;
+       console.log('AVERAGE DB',  dB )
+       return [dW, dB];
+       }
+        async trainModel({model, epochs, alpha, xTrain, yTrain})
+       {
+         let nanoNeuron;
+         const costHistory = [];
+         for (let epoch = 0; epoch < epochs; epoch += 1){
+         const [predictions, cost] = this.forwardPropagation(model, xTrain, yTrain);
+         costHistory.push(cost);
+         const [dW, dB] = this.backwardPropagation(predictions, xTrain, yTrain);
+         nanoNeuron.w += alpha * dW;
+         console.log('nanoNeuron dw',nanoNeuron.w)
+         nanoNeuron.b += alpha * dB;
+         console.log('nanoNeuron dB',nanoNeuron.b)
+       }
+       
+       console.log('trainmodel',this.trainModel({model, epochs, alpha, xTrain, yTrain}))
+        return costHistory;
+       }
+        w=Math.random()
+        b = Math.random();
+    /*  getdata(){
+        var arr = [];
+        for (let i=0; i<10; i++) {
+          arr.push(Array(10).fill(0).map(()=>Math.random()));
+        }
+        return arr
+
       }
-
+      */
+       fetchdata()
+       {
+       }
 }
--- feb-08/Nano-Neuron/src/app/app.module.ts
+++ feb-08/Nano-Neuron/src/app/app.module.ts
@@ -10,12 +10,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { PlotlyViaCDNModule } from 'angular-plotly.js';
+
+import { HttpClientModule } from '@angular/common/http';
 
 
-//import * as PlotlyJS from 'plotly.js/dist/plotly.js';
-//import { PlotlyModule } from 'angular-plotly.js'
+PlotlyViaCDNModule.plotlyVersion = '1.49.4'; // can be `latest` or any version number (i.e.: '1.40.0')
+// tslint:disable-next-line: max-line-length
+PlotlyViaCDNModule.plotlyBundle = 'basic'; // optional: can be null (for full) or 'basic', 'cartesian', 'geo', 'gl3d', 'gl2d', 'mapbox' or 'finance'
 
-//PlotlyModule.plotlyjs = PlotlyJS;
+
 
 @NgModule({
   declarations: [
@@ -23,11 +27,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     ReactiveFormsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    PlotlyViaCDNModule,
     //PlotlyModule
 
   ],
